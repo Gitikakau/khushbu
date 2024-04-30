bb73392e3f340f8d651455af6e72ae19779a5471
Gitikakau <146334680+Gitikakau@users.noreply.github.com>
2024-04-30 21:23:22 +0530

    Update genqr.py

--- genqr.py
+++ genqr.py
@@ -1,6 +1,6 @@
 import qrcode
 
-def generate_qr_code(data, filename):
+def genqr(data, filename):
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -15,27 +15,7 @@ def generate_qr_code(data, filename):
 
 if __name__ == "__main__":
     # Example data for QR code
-    data = "Gurmeet Singh"
+    data = "Gitika Kaushik"
 
     # Generate QR code image
-    generate_qr_code(data, "qrcode.png")rt qrcode
-
-def generate_qr_code(data, filename):
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
-        border=4,
-    )
-    qr.add_data(data)
-    qr.make(fit=True)
-
-    img = qr.make_image(fill_color="black", back_color="white")
-    img.save(filename)
-
-if __name__ == "__main__":
-    # Example data for QR code
-    data = "Gitika"
-
-    # Generate QR code image
-    generate_qr_code(data, "qrcode.png")
+    genqr(data, "qrcode.png")
